f4bca024a47718d035555a1104a7c30b24b7b1b0
white <abhijithkkpurameri@gmail.com>
2023-09-04 00:19:32 +0530

    fav icon added

--- src/Components/Wether/Wether.jsx
+++ src/Components/Wether/Wether.jsx
@@ -7,11 +7,11 @@ import Swal from "sweetalert2";
 import { Hourglass } from "react-loader-spinner";
 function Wether() {
   const [data, setData] = useState({});
-  const [serch, SetSearch] = useState("New york");
+  const [serch, SetSearch] = useState("");
+  const [loader, setLoader] = useState(false);
   const Searching = (e) => {
     SetSearch(e.target.value);
   };
-  const [loader, setLoader] = useState(false);
   const GetWether = () => {
     if (serch.trim()) {
       setLoader(true);
@@ -47,86 +47,96 @@ function Wether() {
       <div className="Wether-mail">
         <div className="OuterDiv">
           <div className="SearchBar">
-            <input type="text" onChange={(e) => Searching(e)} value={serch} />
+            <input
+              type="text"
+              placeholder=" Enter Location"
+              onChange={(e) => Searching(e)}
+              value={serch}
+            />
             <button type="button" onClick={GetWether}>
               Search
             </button>
           </div>
-          {loader===true
-          ?<div className="wetherImage2">
-            <Hourglass
-          visible={loader}
-          height="100"
-          width="100"
-          ariaLabel="hourglass-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          colors={["#ffff", "gray"]}
-        />
-          </div>
-          :<>
-          <div
-            className={
-              data?.current?.condition?.icon ? "wetherImage" : "wetherImage1"
-            }
-          >
-            
-            <img
-              src={
-                data?.current?.condition?.icon
-                  ? data?.current?.condition?.icon
-                  : Search
-              }
-              alt="Not found"
-            />
-          </div>
-          <div className="temp">
-            {data?.current?.condition?.text
-              ? data?.current?.condition?.text
-              : ""}
-          </div>
-          <div className="wetherPlace">
-            <div className="celsious">
-              {data?.current?.temp_c ? data?.current?.temp_c + "°C" : ""}
+          {loader === true ? (
+            <div className="wetherImage2">
+              <Hourglass
+                visible={loader}
+                height="100"
+                width="100"
+                ariaLabel="hourglass-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                colors={["#ffff", "gray"]}
+              />
             </div>
-            <br />
-            <h3>
-              {data?.location?.name
-                ? data?.location?.name
-                : data?.error
-                ? data?.error?.message
-                : "Search a Location"}
-            </h3>
-          </div>
-          <div className="footer">
-            <div className="humudity">
-              {data?.current?.humidity ? (
-                <>
-                  <img src={Humudity} alt="" />
-                  &nbsp;
-                  <b style={{ fontSize: "40px" }}>{data?.current?.humidity}</b>
-                  % <br /> Humudity
-                </>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="wind">
-              {data?.current?.wind_kph ? (
-                <>
-                  <img src={wind} alt="" />
-                  &nbsp;
-                  <b style={{ fontSize: "40px" }}>
-                    {data?.current?.wind_kph}
-                  </b>{" "}
-                  km/h <br /> wind speed
-                </>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
-          </>}
+          ) : (
+            <>
+              <div
+                className={
+                  data?.current?.condition?.icon
+                    ? "wetherImage"
+                    : "wetherImage1"
+                }
+              >
+                <img
+                  src={
+                    data?.current?.condition?.icon
+                      ? data?.current?.condition?.icon
+                      : Search
+                  }
+                  alt="Not found"
+                />
+              </div>
+              <div className="temp">
+                {data?.current?.condition?.text
+                  ? data?.current?.condition?.text
+                  : ""}
+              </div>
+              <div className="wetherPlace">
+                <div className="celsious">
+                  {data?.current?.temp_c ? data?.current?.temp_c + "°C" : ""}
+                </div>
+                <br />
+                <h3>
+                  {data?.location?.name
+                    ? data?.location?.name
+                    : data?.error
+                    ? data?.error?.message
+                    : "Search a location to check wether status."}
+                </h3>
+              </div>
+              <div className="footer">
+                <div className="humudity">
+                  {data?.current?.humidity ? (
+                    <>
+                      <img src={Humudity} alt="" />
+                      &nbsp;
+                      <b style={{ fontSize: "40px" }}>
+                        {data?.current?.humidity}
+                      </b>
+                      % <br /> Humudity
+                    </>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="wind">
+                  {data?.current?.wind_kph ? (
+                    <>
+                      <img src={wind} alt="" />
+                      &nbsp;
+                      <b style={{ fontSize: "40px" }}>
+                        {data?.current?.wind_kph}
+                      </b>{" "}
+                      km/h <br /> wind speed
+                    </>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
